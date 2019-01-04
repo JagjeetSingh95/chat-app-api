f9673c3ce24cae2eeeff23bbfcd09cc5c832a73e
Jagjeet Singh <sjagjeet528@gmail.com>
2019-01-04 15:24:16 +0530

    upadted

--- server.js
+++ server.js
@@ -1,7 +1,34 @@
-const http = require('http');
-const app = require('./app');
+// const http = require('http');
+// const app = require('./app');
 
-const port = process.env.PORT || 3001;
-const server = http.createServer(app);
+// const port = process.env.PORT || 3001;
+// const server = http.createServer(app);
 
-server.listen(port);
+// server.listen(port);
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    console.log(socket.id, 'a user connected');
+    socket.on('disconnect', function(){
+      console.log('user disconnected');
+    });
+
+    socket.on('user', function(user) {
+        console.log(user, "user")
+    })
+
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+      });
+  });
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
